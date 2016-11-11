5df808f60f197e5003a7c7bc2f6b906a154f6460
SrGio <sergionavarronavarro@msn.com>
2016-11-11 09:41:14 +0100

    P04 - Alumnos

--- src/FctBundle/Form/EmpresasType.php
+++ src/FctBundle/Form/EmpresasType.php
@@ -7,6 +7,9 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\ResetType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 
 class EmpresasType extends AbstractType
 {
@@ -17,12 +20,12 @@ class EmpresasType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('nombre')
-            ->add('direccion')
-            ->add('cp')
-            ->add('telefono1')
-            ->add('telefono2')
-            ->add('fechaCreacion')
+            ->add('nombre',TextType::class,array('label'=>'Nombre de la Empresa'))
+            ->add('direccion',TextType::class)
+            ->add('cp',IntegerType::class,array('label'=>'Código Postal'))
+            ->add('telefono1',IntegerType::class,array('label'=>'Teléfono Principal'))
+            ->add('telefono2',IntegerType::class,array('label'=>'Teléfono Secundario'))
+            ->add('fechaCreacion',DateType::class,array('label'=>'Fecha de creación de la Empresa'))
             ->add('guardar',SubmitType::class)
             ->add('borrar', ResetType::class)
         ;
