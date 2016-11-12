c60cab0e9299418d19e855e543cab292a27437f5
SrGio <sirgiona@gmail.com>
2016-11-12 10:06:37 +0100

    P04 - Alumnos y plantilla

---
+++ src/FctBundle/Form/AlumnosType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace FctBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\ResetType;
+
+class AlumnosType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nombre')
+            ->add('apellido1')
+            ->add('apellido2')
+            ->add('ciclo')
+            ->add('guardar',SubmitType::class)
+            ->add('borrar', ResetType::class)
+        ;
+    }
+
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'FctBundle\Entity\Alumnos'
+        ));
+    }
+}
