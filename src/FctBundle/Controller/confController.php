4e7bef87ebb8190096388c9c86bfb601c57bef7f
Sergio <sirgiona@gmail.com>
2017-02-02 11:53:48 +0100

    Examen - Primera subida

---
+++ src/FctBundle/Controller/confController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace FctBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use FctBundle\Entity\conf;
+use FctBundle\Form\confType;
+use Symfony\Component\httpFoundation\Request;
+
+class AlumnosController extends Controller
+{
+    public function confAction()
+    {
+        $repository = $this->getDoctrine()->getRepository('FctBundle:conf');
+        $config = $repository->findAll();
+        return $this->render('FctBundle:conf:config.html.twig',array('config'=>$config));
+    }
+
+    public function addConfAction(Request $request)
+    {
+        $config = new conf();
+        $form= $this->createForm(confType::class,$config);
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+          //
+          //
+          $config=$form->getData();
+          //
+          //
+          $em=$this->getDoctrine()->getManager();
+          $em->persist($config);
+          $em->flush();
+
+          return $this->redirectToRoute('exito_configuracion',array("status"=>"ok"));
+      }
+
+    	return $this->render('FctBundle:conf:addConf.html.twig',array("form"=>$form->createView() ));
+    }
+
+    public function msgExitoAction()
+    {
+        $repository = $this->getDoctrine()->getRepository('FctBundle:conf');
+        $config = $repository->findAll();
+        return $this->render('FctBundle:conf:conf.html.twig',array('config'=>$config));
+    }
+}
